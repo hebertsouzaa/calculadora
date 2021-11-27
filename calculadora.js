2d5f783abe653d4f7d3a364dd072e5d9639f81ae
hebertsouzaa <hebert.viniciuss@outlook.com.br>
2021-11-27 11:32:11 -0300

    calculadora

---
+++ calculadora.js
@@ -0,0 +1,8 @@
+function calcu(a,b){
+    console.log('Soma =',(a+b))
+    console.log('Subtração =',(a-b))
+    console.log('Multiplicação =',(a*b))
+    console.log('divisão = ', Math.ceil(a/b) )
+}
+
+calcu(454566,34654)
